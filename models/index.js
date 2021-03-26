9776e1a84af74010302198e2e9ccb2cdafec0523
dlashinsky <da.lashinsky@gmail.com>
2021-03-26 15:53:16 -0700

    config deployment

--- models/index.js
+++ models/index.js
@@ -1,20 +1,56 @@
 const mongoose = require('mongoose')
 
+if(process.env.NODE_ENV === 'development'){
 
-const MONGO_URI=process.env.MONGO_URI || 'mongodb://localhost/related'
-mongoose.connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-})
+    const MONGO_URI=process.env.MONGO_URI || 'mongodb://localhost/related'
+    mongoose.connect(MONGO_URI, {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+    })
+    
+    const db = mongoose.connection; 
+    
+    db.once('open', () =>{
+        console.log(`Mongoose is running ${db.host}: ${db.port}`)
+    })
+    
+    db.on('error', (err) =>{
+        console.error(`Mongoose IS NOT connected\n ${err}`)
+    })
 
-const db = mongoose.connection; 
+} else {
+    
 
-db.once('open', () =>{
-    console.log(`Mongoose is running ${db.host}: ${db.port}`)
-})
+    const MongoClient = require('mongodb').MongoClient;
+    const uri = process.env.ATLAS_URI
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+    mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+        useUnifiedTopology: true
+    })
+    
+    const db = mongoose.connection; 
+    
+    db.once('open', () =>{
+        console.log(`Mongoose is running ${db.host}: ${db.port}`)
+    })
+    
+    db.on('error', (err) =>{
+        console.error(`Mongoose IS NOT connected\n ${err}`)
+    })
 
-db.on('error', (err) =>{
-    console.error(`Mongoose IS NOT connected\n ${err}`)
-})
+}
+
+// MONGO USER CRUDENTIALS
+//USERNAME:   
+        // related_user
+    // p
